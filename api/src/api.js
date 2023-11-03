d20e10a59597b78e3492db6dd8e16022c02b9a63
CoinBR <coinbr@gmail.com@>
2023-11-03 11:55:01 -0300

    Refactor to typescript, and separate api from ui

---
+++ api/src/api.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const env = process.env;
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = env.VITE_API_PORT;
+
+const UI_URL = `http://${env.VITE_UI_HOST}:${env.VITE_UI_PORT}`
+
+const STATE_FILE = './db/state.json';
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+
+app.use(cors({
+  origin: UI_URL,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
+
+function emptyState() {
+  return {
+    day: null,
+    countdowns: [],
+  };
+}
+
+function readJsonFile(filePath) {
+  const rawData = fs.readFileSync(STATE_FILE);
+  return JSON.parse(rawData);
+}
+
+let state = fs.existsSync(STATE_FILE)
+  ? readJsonFile(STATE_FILE)
+  : emptyState()
+
+if (fs.existsSync(STATE_FILE)) {
+} else { }
+
+app.get('/state', (req, res) => {
+  res.send(state);
+});
+
+app.put('/state', (req, res) => {
+  state = req.body;
+  fs.writeFileSync(STATE_FILE, JSON.stringify(state));
+  res.send({ status: 'OK' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Spread Sweat API started on port ${PORT}`);
+});
---
+++ ui/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const env = import.meta.env;
+const API_URL = `http://${env.VITE_API_HOST}:${env.VITE_API_PORT}`
+
+const countdownStateEndpoint = `${API_URL}/state`;
+
+import Countdowns from './components/Countdowns.vue';
+</script>
+
+<template>
+  <Countdowns :api-state-endpoint="countdownStateEndpoint" />
+</template>
+
+<style scoped>
+body {
+  background-color: #333;
+  color: #f5f5f5;
+  font-family: Arial, sans-serif;
+}
+</style>
